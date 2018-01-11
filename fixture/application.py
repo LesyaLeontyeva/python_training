76e54f951e12a27cf6a506110e692eb05470a9f5
Leontyeva <orchid345617@gmail.com>
2018-01-11 18:23:28 +0300

    Выделен вспомогательный класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,20 +1,21 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
-
-    def logout(self):
+    def open_home_page(self):
         wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        wd.get("http://localhost/addressbook/")
 
-
-    def return_to_groups_page(self):
+    def open_groups_page(self):
         wd = self.wd
-        wd.find_element_by_link_text("group page").click()
+        # open groups page
+        wd.find_element_by_link_text("groups").click()
 
     def create_group(self, group):
         wd = self.wd
@@ -35,25 +36,9 @@ class Application:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-    def open_groups_page(self):
+    def return_to_groups_page(self):
         wd = self.wd
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_link_text("group page").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,19 +6,19 @@ from fixture.application import Application
 @pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy())
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_group(app):
-        app.login(username ="admin", password="secret")
-        app.create_group(Group(name="oioioio", header="popuyuyu", footer="ytytyty"))
-        app.logout()
+    app.session.login(username ="admin", password="secret")
+    app.create_group(Group(name="oioioio", header="popuyuyu", footer="ytytyty"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-        app.login(username ="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+    app.session.login(username ="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.session.logout()
 
 
