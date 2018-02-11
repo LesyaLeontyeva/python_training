60e0953af89bf788bac159c2ae834a2ef0319bd6
Leontyeva <orchid345617@gmail.com>
2018-02-11 18:07:21 +0300

    Доработала тест для удаления контактов.

--- model/contact2.py
+++ model/contact2.py
@@ -22,5 +22,12 @@ class Contact2:
         self.id = id
         self.name = name
 
+    def __repr__(self):
+        return "%s:%s" % (self.id,self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
+
+
 
 
