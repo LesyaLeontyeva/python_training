6b5c450dd12a54be2c9c9645bb8c9eb00b6b6379
Leontyeva <orchid345617@gmail.com>
2018-03-07 15:46:44 +0300

    Задание №21: Переделать тесты для проверки информации о контактах на главной странице

--- test/test_phones.py
+++ test/test_phones.py
@@ -22,6 +22,24 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
 
 
+def test_information_from_db(app, db, data_contacts):
+    if app.contact.count() == 0:
+        app.contact.add_contact(data_contacts)
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+    contact_from_home_page.sort(key=Contact2.id_or_max)
+    contact_from_db.sort(key=Contact2.id_or_max)
+    assert len(contact_from_home_page) == len(contact_from_db)
+    for i in range(len(contact_from_home_page)):
+        assert contact_from_home_page[i].parametr['all_phone_from_home_page'] == merge_phones_like_on_home_page(
+            contact_from_db[i])
+        assert contact_from_home_page[i].parametr['all_emails_from_home_page'] == merge_emails_like_on_home_page(
+            contact_from_db[i])
+        assert contact_from_home_page[i].parametr['lastname'] == contact_from_db[i].parametr['lastname']
+        assert contact_from_home_page[i].parametr['firstname'] == contact_from_db[i].parametr['firstname']
+        assert contact_from_home_page[i].parametr['address'] == contact_from_db[i].parametr['address']
+
+
 def clear(s):
     return re.sub("[() -]", "", s)
 
